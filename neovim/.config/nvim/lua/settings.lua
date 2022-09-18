b24aca6bc3dd34927e5fe6729c8aed621fbe6b45
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2022-09-18 11:29:23 -0400

    global settings in lua for neovim

--- neovim/.config/nvim/lua/settings.lua
+++ neovim/.config/nvim/lua/settings.lua
@@ -1,18 +1,36 @@
-
-vim.opt.background = "dark";
-
 --Global settings
 vim.opt.tabstop = 4
 vim.opt.shiftwidth = 4
-vim.opt.expandtab = 4
+vim.opt.expandtab = true
 vim.opt.ignorecase = true
 vim.opt.relativenumber = true
 vim.opt.number = true
---vim.opt.path+="**"
+vim.opt.numberwidth = 1
+vim.opt.path = vim.opt.path + "**"
 vim.opt.virtualedit = "onemore"
---vim.opt.completeopt-= "preview"
+vim.opt.completeopt = vim.opt.completeopt - "preview"
+vim.opt.shortmess = vim.opt.shortmess + "c"
+vim.opt.signcolumn = "yes"
+vim.opt.cmdheight = 2
 
--- Cursor option
+-- Cursor options
+cursors = {
+    ["horizontal"] = "a:hor50-blinkwait700-blinkoff400-blinkon250", -- underline cursor
+    ["block"] = "a:block-blinkwait700-blinkoff400-blinkon250",
+    ["beam"] = "a:ver10-blinkwait700-blinkoff400-blinkon250",
+}
+
 vim.api.nvim_create_autocmd(
 {"VimLeave"},
-{ command = vim.opt.guicursor="a:hor50-blinkwait700-blinkoff400-blinkon250" })
+{ callback = function() 
+    -- vim.opt.guicursor = cursors["beam"] -- comment out the return for petalchaser
+    return
+end })
+
+-- Save cursor position
+vim.api.nvim_create_autocmd(
+{"BufReadPost"},
+{ callback = function() 
+    vim.fn.setpos(".", vim.fn.getpos("'\""))
+end })
+
