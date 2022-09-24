b6ff8565b06e1592621c3ecf217691255971d294
Mohamed Lemine Ould El-Hadj <mohamedlemineelhadj@outlook.com>
2022-09-24 18:29:41 -0400

    Lualine with one of the default examples

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,4 +1,5 @@
 --neovim
 require('settings') -- lua/settings.lua
 require('mappings') -- lua/mappings.lua
+require('plugins') -- lua/plugins.lua
 
--- neovim/.config/nvim/lua/plugins.lua
+++ neovim/.config/nvim/lua/plugins.lua
@@ -2,4 +2,10 @@
 return require("packer").startup(function()
     use("wbthomason/packer.nvim")
 
+    use {
+    'nvim-lualine/lualine.nvim',
+    config = function() require 'statusline/evil_lualine' end,
+    requires = { 'kyazdani42/nvim-web-devicons', opt = true }
+    }
+
 end)
