5ecb3372d63129f5653076d36d5f2c3e553f09a8
Mewt R <25155631+MewtR@users.noreply.github.com>
2023-12-20 04:54:12 -0500

    Fixing vimwiki not detecting my wiki

--- neovim/.config/nvim/init.lua
+++ neovim/.config/nvim/init.lua
@@ -1,12 +1,11 @@
 --neovim
 require('settings') -- lua/settings.lua
 require('mappings') -- lua/mappings.lua
+require('plugins/vimwiki') -- lua/plugins/vimwiki.lua
 require('plugins') -- lua/plugins.lua
 
 require('plugins/tokyonight') -- lua/plugins/tokyonight.lua
 
 require('plugins/telescope') -- lua/plugins/telescope.lua
-
-require('plugins/vimwiki') -- lua/plugins/vimwiki.lua
 
 require('plugins/lspconfig') -- lua/plugins/lspconfig.lua
